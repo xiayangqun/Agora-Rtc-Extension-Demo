27a0bbf84980d3cdd2779542530d04177a247ecc
xiayangqun <444746024@qq.com>
2026-05-19 20:09:28 +0800

    camera scene finish (#3)
    * localVideo show
    
    * camera scene finish
    
    ---------
    
    Co-authored-by: xiayangqun <xyq377x@163.com>

---
+++ assets/srcipts/base/VideoItemBase.ts
@@ -0,0 +1,167 @@
+import { Label } from 'cc';
+import { Mesh } from 'cc';
+import { Texture2D, SpriteFrame, Rect, Size, UITransform } from 'cc';
+import { Material } from 'cc';
+import { MeshRenderer } from 'cc';
+import { Sprite } from 'cc';
+import { _decorator, Component, Node } from 'cc';
+import { IRtcEngineEx, RtcConnection, VIDEO_SOURCE_TYPE, VideoCanvas } from 'db://agora-rtc-extension-for-cocos-creator/agora-rtc';
+const { ccclass, property } = _decorator;
+
+@ccclass('VideoItemBase')
+export class VideoItemBase extends Component {
+
+    @property(Label)
+    public belongLabel: Label = null;
+
+    @property(Label)
+    public infoLabel: Label = null;
+
+    @property(Sprite)
+    public videoSprite: Sprite = null;
+
+    @property(MeshRenderer)
+    public videoMeshRenderer: MeshRenderer = null;
+
+    protected rtcEngine: IRtcEngineEx = null;
+
+    protected texture: Texture2D = null;
+
+    protected spriteFrame: SpriteFrame = null;
+
+    protected material: Material = null;
+
+    protected canvasClone: VideoCanvas = null;
+
+    protected connectionClone: RtcConnection = null;
+
+
+    setupVideoCanvas(rtcEngine: IRtcEngineEx, canvas: VideoCanvas, connection?: RtcConnection) {
+        this.canvasClone = { ...canvas };
+        if (connection) {
+            this.connectionClone = { ...connection };
+        }
+
+        this.rtcEngine = rtcEngine;
+        if (canvas.uid == 0) {
+            //is local video canvas
+            this.belongLabel.string = "Local";
+            if (canvas.mediaPlayerId != 0) {
+                this.infoLabel.string = `MediaPlayer ID: ${canvas.mediaPlayerId}`;
+            }
+            else {
+                this.infoLabel.string = VIDEO_SOURCE_TYPE[canvas.sourceType];
+            }
+
+            this.createTextureAndAttachToSelf();
+            canvas.view = this.texture;
+            rtcEngine.setupLocalVideo(canvas, (width, height) => {
+                this.resize(width, height);
+            });
+        }
+        else {
+            //is remote video canvas
+            this.belongLabel.string = "Remote";
+           
+            this.createTextureAndAttachToSelf();
+            canvas.view = this.texture;
+            if (connection) {
+                this.infoLabel.string = `ex: Channel ID: ${connection.channelId}, UID: ${connection.localUid}`;
+                rtcEngine.setupRemoteVideoEx(canvas, connection, (width, height) => {
+                    this.resize(width, height);
+                });
+            } else {
+                this.infoLabel.string = `main UID: ${canvas.uid}`;
+                rtcEngine.setupRemoteVideo(canvas, (width, height) => {
+                    this.resize(width, height);
+                });
+            }
+        }
+    }
+
+    unsetupVideoCanvas(canvas: VideoCanvas, connection?: RtcConnection) {
+        canvas.view = null;
+        if (canvas.uid == 0) {
+            this.rtcEngine.setupLocalVideo(canvas);
+        } else {
+            if (connection) {
+                this.rtcEngine.setupRemoteVideoEx(canvas, connection);
+            } else {
+                this.rtcEngine.setupRemoteVideo(canvas);
+            }
+        }
+    }
+
+    isSameVideoItem(canvas: VideoCanvas, connection?: RtcConnection): boolean {
+        if (canvas.uid != this.canvasClone.uid) {
+            return false;
+        }
+        if (canvas.sourceType != this.canvasClone.sourceType) {
+            return false;
+        }
+        if (canvas.sourceType == VIDEO_SOURCE_TYPE.VIDEO_SOURCE_MEDIA_PLAYER &&
+            this.canvasClone.sourceType == VIDEO_SOURCE_TYPE.VIDEO_SOURCE_MEDIA_PLAYER) {
+            if (canvas.mediaPlayerId != this.canvasClone.mediaPlayerId) {
+                return false;
+            }
+        }
+
+        if (connection && this.connectionClone) {
+            if (connection.channelId != this.connectionClone.channelId) {
+                return false;
+            }
+            if (connection.localUid != this.connectionClone.localUid) {
+                return false;
+            }
+        }
+
+        if (connection == null && this.connectionClone != null) {
+            return false;
+        }
+
+        if(connection != null && this.connectionClone == null){
+            return false;
+        }
+        return true;
+    }
+
+    createTextureAndAttachToSelf(): void {
+        this.texture = new Texture2D();
+
+        this.spriteFrame = new SpriteFrame();
+        this.spriteFrame.packable = false;
+        this.spriteFrame.texture = this.texture;
+
+        this.videoSprite.spriteFrame = this.spriteFrame;
+        this.material = this.videoMeshRenderer.getMaterialInstance(0);
+        this.material.setProperty("mainTexture", this.texture);
+    }
+
+    resize(width: number, height: number) {
+        console.log("resize", width, height);
+
+        // if (this.spriteFrame) {
+        //     this.spriteFrame.packable = false;
+        //     (this.spriteFrame as unknown as { _resetDynamicAtlasFrame?: () => void })._resetDynamicAtlasFrame?.();
+        // }
+
+        this.spriteFrame.reset({
+            texture: this.texture,
+            rect: new Rect(0, 0, width, height),
+            originalSize: new Size(width, height),
+        });
+        // this.videoSprite.setTextureDirty?.();
+        this.videoSprite.markForUpdateRenderData?.();
+        let spriteTrans = this.videoSprite.getComponent(UITransform);
+        let spriteWidth = spriteTrans.height * width / height;
+        spriteTrans.width = spriteWidth;
+
+        this.material.setProperty("mainTexture", this.texture);
+    }
+
+    protected onDestroy(): void {
+        this.texture.destroy();
+        this.spriteFrame.destroy();
+    }
+}
+
---
+++ assets/srcipts/camera/CameraCanvas.ts
@@ -0,0 +1,172 @@
+import { _decorator, Component, Prefab, instantiate } from "cc";
+import {
+    IRtcEngineEventHandler,
+    IRtcEngineEx,
+    createRtcEngine,
+    LOG_LEVEL,
+    RtcEngineContext,
+    VIDEO_MIRROR_MODE_TYPE,
+    RtcConnection,
+    RENDER_MODE_TYPE,
+    VIDEO_SOURCE_TYPE,
+    VIDEO_VIEW_SETUP_MODE,
+    VIDEO_MODULE_POSITION,
+    CHANNEL_PROFILE_TYPE,
+    AREA_CODE,
+    AUDIO_SCENARIO_TYPE,
+    VideoCanvas,
+    USER_OFFLINE_REASON_TYPE
+} from "db://agora-rtc-extension-for-cocos-creator/agora-rtc";
+import { VideoSprite } from "../prefab/VideoSprite";
+import { BaseCanvas } from "../base/BaseCanvas";
+import { app } from "electron";
+
+const { ccclass, property } = _decorator;
+
+class CameraCanvasRtcEngineEventHandler extends IRtcEngineEventHandler {
+    _cameraCanvas: CameraCanvas = null;
+    constructor(cameraCanvas: CameraCanvas) {
+        super();
+        this._cameraCanvas = cameraCanvas;
+    }
+
+    onJoinChannelSuccess(connection: RtcConnection, elapsed: number): void {
+        console.log(" onJoinChannelSuccess, connection: ", connection);
+        let canvas: VideoCanvas = {
+            uid: 0,
+            view: null,
+            sourceType: VIDEO_SOURCE_TYPE.VIDEO_SOURCE_CAMERA,
+            mediaPlayerId: 0,
+        };
+        this._cameraCanvas.videoContent.createVideoItem(this._cameraCanvas.rtcEngine, canvas, null);
+    }
+
+    onUserJoined(connection: RtcConnection, remoteUid: number, elapsed: number): void {
+        this._cameraCanvas.logContent.log("onUserJoined, remoteUid: ", remoteUid);
+        let canvas: VideoCanvas = {
+            uid: remoteUid,
+            view: null,
+            sourceType: VIDEO_SOURCE_TYPE.VIDEO_SOURCE_CAMERA,
+            mediaPlayerId: 0,
+        };
+        this._cameraCanvas.videoContent.createVideoItem(this._cameraCanvas.rtcEngine, canvas, null);
+    }
+
+    onLeaveChannel(connection: RtcConnection): void {
+        this._cameraCanvas.logContent.log("onLeaveChannel, connection: ", connection);
+    }
+
+    onUserOffline(connection: RtcConnection, remoteUid: number, reason: USER_OFFLINE_REASON_TYPE): void {
+        this._cameraCanvas.logContent.log("onUserOffline, remoteUid: ", remoteUid);
+        let canvas: VideoCanvas = {
+            uid: remoteUid,
+            view: null,
+            sourceType: VIDEO_SOURCE_TYPE.VIDEO_SOURCE_CAMERA,
+            mediaPlayerId: 0,
+        };
+        this._cameraCanvas.videoContent.destroyVideoItem(canvas, null);
+    }
+}
+
+@ccclass("CameraCanvas")
+export class CameraCanvas extends BaseCanvas {
+    public uid: number = 1001;
+
+
+    async createRtcEngine(): Promise<void> {
+        this.rtcEngine = createRtcEngine();
+        await this.initAppIdInfo();
+        const appIdInfo = this.appIdInfo;
+        let config: RtcEngineContext = {
+            eventHandler: new CameraCanvasRtcEngineEventHandler(this),
+            appId: appIdInfo.appId,
+            context: 0,
+            channelProfile: CHANNEL_PROFILE_TYPE.CHANNEL_PROFILE_GAME,
+            license: "",
+            audioScenario: AUDIO_SCENARIO_TYPE.AUDIO_SCENARIO_AI_CLIENT,
+            areaCode: AREA_CODE.AREA_CODE_CN | AREA_CODE.AREA_CODE_AS,
+            logConfig: {
+                filePath: "log.txt",
+                fileSizeInKB: 1024,
+                level: LOG_LEVEL.LOG_LEVEL_INFO,
+            },
+            useExternalEglContext: false,
+            domainLimit: false,
+            autoRegisterAgoraExtensions: false,
+        };
+        let erroCode = 0;
+        erroCode = await this.rtcEngine.initialize(config);
+        if (erroCode !== 0) {
+            this.logContent.error("initialize failed, errorCode: ", erroCode);
+            return;
+        }
+        else {
+            this.logContent.log("initialize success");
+        }
+    }
+
+    async startPreview(): Promise<void> {
+        let errorCode = await this.rtcEngine.startPreview();
+        if (errorCode == 0) {
+            this.logContent.log("startPreview success");
+            this.videoContent.createVideoItem(this.rtcEngine, {
+                uid: 0,
+                view: null,
+                sourceType: VIDEO_SOURCE_TYPE.VIDEO_SOURCE_CAMERA,
+                mediaPlayerId: 0,
+            }, null);
+        }
+        else {
+            this.logContent.error("startPreview failed, errorCode: ", errorCode);
+        }
+    }
+
+    async joinChannel(): Promise<void> {
+        let erroCode = await this.rtcEngine.enableVideo();
+        if (erroCode !== 0) {
+            this.logContent.error(" enableVideo failed, errorCode: ", erroCode);
+        }
+        else {
+            this.logContent.log("enableVideo success");
+        }
+
+        erroCode = await this.rtcEngine.setRtcVideoDebugViewEnabled(true);
+        if (erroCode !== 0) {
+            this.logContent.error("setRtcVideoDebugViewEnabled failed, errorCode: ", erroCode);
+        }
+        else {
+            this.logContent.log("setRtcVideoDebugViewEnabled success");
+        }
+
+        erroCode = await this.rtcEngine.joinChannel(
+            this.appIdInfo.token,
+            this.appIdInfo.channelId,
+            "",
+            this.uid
+        );
+        if (erroCode !== 0) {
+            this.logContent.error(" joinChannel failed, errorCode: ", erroCode);
+            return;
+        }
+        else {
+            this.logContent.log(" joinChannel success");
+        }
+    }
+
+    async leaveChannel(): Promise<void> {
+        let errorCode = await this.rtcEngine.leaveChannel();
+        if (errorCode !== 0) {
+            this.logContent.error("leaveChannel failed, errorCode: ", errorCode);
+        }
+        else {
+            this.logContent.log(" leaveChannel success");
+        }
+    }
+
+    async releaseRtcEngine(): Promise<void> {
+        await this.rtcEngine.release(true);
+        this.rtcEngine = null;
+        this.videoContent.clear();
+        this.logContent.log("releaseRtcEngine success");
+    }
+}
